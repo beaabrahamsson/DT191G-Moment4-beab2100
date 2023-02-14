c0a47deca6be71dc962186d01075804aadd4be12
Beatrice Abrahamsson <beaabrahamsson@hotmail.com>
2023-02-14 15:02:07 +0100

    first commit

---
+++ Models/SongModel.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DT191G_Moment4_beab2100 {
+    public class Song {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Ange en artist")]
+        public string? Artist { get; set; }
+        [Required(ErrorMessage = "Ange en titel")]
+        public string? Titel { get; set; }
+        [Required(ErrorMessage = "Ange en längd")]
+        public int Lenght { get; set; }
+        [Required(ErrorMessage = "Ange en kategori")]
+        public string? Category { get; set; }
+    }
+}
